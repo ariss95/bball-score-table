1e3ee42357726a2897f4314481e94b3e68bccd5c
ariss95 <cse32538@cs.uoi.gr>
2020-10-07 22:57:10 +0300

    added navbar, should fix it's display

---
+++ settings.js
@@ -0,0 +1,8 @@
+function showBar(){
+    document.getElementById("navbar").style.display = "block";
+    document.getElementById("show-navbar").style.display = "none";
+}
+function hideBar(){
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("show-navbar").style.display = "block";
+}
